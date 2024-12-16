9e2530bd271bb2cff96af3aa5633289631edb64e
Petru Binzari <petru_binzari@yahoo.com>
2024-12-16 11:03:29 +0200

    fix form uploader

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -15,7 +15,7 @@ namespace MonsieurBiz\SyliusRichEditorPlugin\Controller;
 
 use MonsieurBiz\SyliusRichEditorPlugin\Exception\UiElementNotFoundException;
 use MonsieurBiz\SyliusRichEditorPlugin\UiElement\RegistryInterface;
-use MonsieurBiz\SyliusRichEditorPlugin\Uploader\FileUploader;
+use MonsieurBiz\SyliusRichEditorPlugin\Uploader\FileUploaderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\FileType as NativeFileType;
 use Symfony\Component\Form\FormInterface;
@@ -134,13 +134,13 @@ class FormController extends AbstractController
      * Validate submitted data and return an UI Element JSON if everything is OK.
      *
      * @param Request $request
-     * @param FileUploader $fileUploader
+     * @param FileUploaderInterface $fileUploader
      * @param string $code
      * @param bool $isEdition
      *
      * @return Response
      */
-    public function submitAction(Request $request, FileUploader $fileUploader, string $code, bool $isEdition): Response
+    public function submitAction(Request $request, FileUploaderInterface $fileUploader, string $code, bool $isEdition): Response
     {
         // Find UI Element from type
         try {
@@ -198,12 +198,12 @@ class FormController extends AbstractController
      * Build a new form data array with the uploaded file path instead of files, or current filenames on edition.
      *
      * @param FormInterface $form
-     * @param FileUploader $fileUploader
+     * @param FileUploaderInterface $fileUploader
      * @param mixed $requestData
      *
      * @return array|mixed|string
      */
-    private function processFormData(FormInterface $form, FileUploader $fileUploader, $requestData)
+    private function processFormData(FormInterface $form, FileUploaderInterface $fileUploader, $requestData)
     {
         // No child, end of recursivity, return form value or uploaded file path
         if (!\count($form->all())) {
@@ -221,12 +221,12 @@ class FormController extends AbstractController
 
     /**
      * @param FormInterface $form
-     * @param FileUploader $fileUploader
+     * @param FileUploaderInterface $fileUploader
      * @param array|string $requestData
      *
      * @return array|mixed|string
      */
-    private function processFormDataWithoutChild(FormInterface $form, FileUploader $fileUploader, $requestData)
+    private function processFormDataWithoutChild(FormInterface $form, FileUploaderInterface $fileUploader, $requestData)
     {
         if ($form->isValid() && $form->getData() instanceof UploadedFile) {
             // Upload image selected by user
